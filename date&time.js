c6ac56e9feac159d0a3a88d83a4aaa3b57fdc578
gk-svg <gk32239@gmail.com>
2020-05-19 17:23:33 +0530

    JS basics

---
+++ date&time.js
@@ -0,0 +1,8 @@
+console.log('This si date and time')
+let today= new Date();
+let a=today.getDate()
+a=today.getDay()
+a=today.getTime()
+a=today.getTimezoneOffset()
+a=today.getMonth()
+console.log(a);
---
+++ math.js
@@ -0,0 +1,16 @@
+console.log('This is math module');
+let x=3;
+let y=10;
+let z;
+z=x+y;
+z=x-y;
+z=x*y;
+z=y/x;
+z=Math.pow(2,2)
+z=Math.sqrt(16)
+z=Math.floor(2.3)
+z=Math.ceil(2.3)
+z=Math.abs(-5)
+z=100*Math.random()
+z=10+(100-10)*Math.random()
+console.log(z);
