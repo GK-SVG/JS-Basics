92ae11e0a88ab29bdb13343eb432b441bae38525
gk-svg <gk32239@gmail.com>
2020-05-22 10:04:10 +0530

    asynchronous in js

---
+++ asycn.js
@@ -0,0 +1,10 @@
+// for (let i=0;i<4000;i++) {
+//     console.log(i);
+// }
+// console.log("Done printing");
+setTimeout(() => {
+    for (let i=0;i<4000;i++) {
+        console.log(i);
+    }
+}, 100);
+console.log("Done printing");
