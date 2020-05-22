eb6527c1902f1ea86c762e49104b042a8c55106f
gk-svg <gk32239@gmail.com>
2020-05-22 21:10:31 +0530

    get request in ajax

--- Ajax/ajax.js
+++ Ajax/ajax.js
@@ -1,1 +1,33 @@
 console.log('this is ajax')
+let fetchBtn=document.getElementById('fetchBtn')
+fetchBtn.addEventListener('click',buttonClickHandler);
+
+function buttonClickHandler(){
+    console.log('You are clicked fetchBtn');
+    
+    //Instantiating the xhr object
+    const xhr= new XMLHttpRequest();
+
+    //open the object
+    xhr.open('GET','test.txt',true);
+    //xhr.open('GET','https://jsonplaceholder.typicode.com/todos/1',true);
+    xhr.open('GET','https://jsonplaceholder.typicode.com/todos/1',false);
+    // xhr.send();
+    // console.log('we are done');
+    
+     //onprogress for loader or spinner
+    xhr.onprogress=function(){
+        console.log('On nprogress');
+    }
+
+    xhr.onload = function () {
+        if (this.status===200) {
+            console.log(this.responseText);  
+        } 
+        else{
+            console.log('some error occured')
+        }
+    }
+    xhr.send();
+    console.log('we are done');
+}
